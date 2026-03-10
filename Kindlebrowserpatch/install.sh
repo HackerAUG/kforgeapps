9375353b74a2be93be38995337b829606b069da8
HackerAUG <augustinejameslinton@gmail.com>
2026-03-10 12:54:07 -0600

    Refactor install.sh for better clarity and functionality
    Updated the install script to use a variable for the download URL and improved the unzip process.

--- Kindlebrowserpatch/install.sh
+++ Kindlebrowserpatch/install.sh
@@ -2,20 +2,21 @@
 
 set -e
 
-TMPDIR=/mnt/us/KFPM-Temporary
-EXTDIR=/mnt/us/extensions
+TMPDIR="/mnt/us/KFPM-Temporary"
+EXTDIR="/mnt/us/extensions"
+ZIP_URL="https://github.com/emilypeto/KindleBrowserPatch/releases/latest/download/kindle_browser_patch-2.1.0-armhf.zip"
+
 mkdir -p "$TMPDIR"
 mkdir -p "$EXTDIR"
 
-# 1. Download directly to a consistent name
-curl -fSL --progress-bar -o "$TMPDIR/KShips.zip" https://github.com/emilypeto/KindleBrowserPatch/releases/latest/download/kindle_browser_patch-2.1.0-armhf.zip
+curl -fSL -o "$TMPDIR/kbp.zip" "$ZIP_URL"
+unzip -oq "$TMPDIR/kbp.zip" -d "$TMPDIR"
 
-# 2. Unzip directly into the extensions folder
-unzip -oq "$TMPDIR/patch.zip" -d "$EXTDIR"
+cp -r "$TMPDIR/kindle_browser_patch-2.1.0-armhf" "$EXTDIR/"
 
-# 3. Cleanup
 rm -rf "$TMPDIR"
 
 exit 0
+
 
 
