0ff2f45a29bf616580673c6da4eba327ce3abb0a
HackerAUG <augustinejameslinton@gmail.com>
2026-03-10 11:27:44 -0600

    Update install.sh to copy files to extensions directory
    Added commands to copy extracted files to the extensions directory.

--- Kindlebrowserpatch/install.sh
+++ Kindlebrowserpatch/install.sh
@@ -10,7 +10,11 @@ curl -fSL --progress-bar -o "$TMPDIR/kinamp.zip" https://github.com/emilypeto/Ki
 unzip -oq "$TMPDIR/kindle_browser_patch-2.1.0-armhf.zip
 " -d /mnt/us
 
+cd kindle_browser_patch-2.1.0-armhf
+cp -r /mnt/us/KFPM-Temporary /mnt/us/extensions/
+
 # Cleanup
 rm -rf "$TMPDIR"
 
 exit 0
+
